9c95e168f1b8a2f6f3f2a26932d49df393b9fd96
LucasFelicianoATeixeira <lucasfeliciano326@gmail.com>
2020-02-20 11:32:38 -0300

    alteraçao

---
+++ Senai.Peoples/Senai.Peoples/Interfaces/IFuncionariosRepository.cs
@@ -0,0 +1,13 @@
+﻿using Senai.Peoples.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.Interfaces
+{
+    interface IFuncionariosRepository
+    {
+        List<FuncionarioDomain> Listar();
+    }
+}
---
+++ Senai.Peoples/Senai.Peoples/Repositories/FuncionariosRepository.cs
@@ -0,0 +1,24 @@
+﻿using Senai.Peoples.Domain;
+using Senai.Peoples.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.Repositories
+{
+    public class FuncionariosRepository : IFuncionariosRepository
+    {
+        string conexao = "Data Source = DEV18\\SQLEXPRESS; initial catalog = M_Peoples; user Id = sa; pwd=sa@132";
+
+
+        public List<FuncionarioDomain> Listar(List<FuncionarioDomain> funcionarios)
+        {
+           using(SqlConnection con = new SqlConnection(conexao))
+            {
+                string command = "SELECT * FROM Funcionarios"
+            }
+        }
+    }
+}
